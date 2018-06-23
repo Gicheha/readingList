4367ae467210b93c0c098e316174678cb6835c39
Gicheha <geoffreygicheha@gmail.com>
2018-06-24 01:08:50 +0300

    Complete Application

--- src/main/java/com/Gicheha/readingList/ReadingListApplication.java
+++ src/main/java/com/Gicheha/readingList/ReadingListApplication.java
@@ -3,6 +3,7 @@ package com.Gicheha.readingList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ReadingListApplication {
 
